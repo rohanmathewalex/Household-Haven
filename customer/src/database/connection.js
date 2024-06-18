10084a6bc1ca03dd2f4e7674244c394040f258f3
rohanmathewalex <rohanmathewalex6674@gmail.com>
2024-06-18 01:50:06 -0400

    microservice for customer folder resturctured

---
+++ customer/src/database/connection.js
@@ -0,0 +1,14 @@
+const mongoose = require('mongoose');
+const { DB_URL } = require('../config');
+
+module.exports = async () => {
+    try {
+      await mongoose.connect(DB_URL);
+      console.log('Db Connected');
+    } catch (error) {
+      console.error('Error ============ ON DB Connection');
+      console.log(error);
+    }
+  };
+
+ 
